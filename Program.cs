9da05e7506531ae54c48d0b927c00fc5a137f84b
bengin34 <becaglar3434@gmail.com>
2023-09-12 22:48:41 +0200

    replace

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿string aFriend = "Kendra";
-Console.WriteLine($"Hello {aFriend}"); //string interpolation
+﻿string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Kendra");
+
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
+
+Console.WriteLine(sayHello);
+
+
+// string greeting = "      Hello World!       ";
+// Console.WriteLine($"[{greeting}]");
+
+// string trimmedGreeting = greeting.TrimStart();
+// Console.WriteLine($"[{trimmedGreeting}]");
+
+// trimmedGreeting = greeting.TrimEnd();
+// Console.WriteLine($"[{trimmedGreeting}]");
+
+// trimmedGreeting = greeting.Trim();
+// Console.WriteLine($"[{trimmedGreeting}]");
+
+// string aFriend = "Kendra";
+// Console.WriteLine($"Hello {aFriend}"); //string interpolation
+
+
+// string firstFriend = "Maria";
+// string secondFriend = "Engin";
+// Console.WriteLine($"The name {firstFriend} has {secondFriend}");
+// Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+// Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
